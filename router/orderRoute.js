f4f582f00767cfd7b7a6952507789badb1cd87ff
SouvikDas2002 <dev.souvik2002@gmail.com>
2023-11-18 01:05:58 +0530

    Update multiple orders

--- router/orderRoute.js
+++ router/orderRoute.js
@@ -2,6 +2,7 @@ const order = require("express").Router();
 const { ObjectId } = require("mongodb");
 let data = require("../mongodb/connection.js");
 
+// *whislist //----
 order.get("/wishlist", async (req, res) => {
     let cartNumber=await data.collection("cart").find({user:req.session.email}).count();
     let ordersNumber=await data.collection("orders").find({primaryEmail:req.session.email}).count();
@@ -30,6 +31,35 @@ order.get("/wishlist/:productname", async (req, res) => {
     // console.log(item)
   res.redirect(`/users/productdetails/${item[0].info[0]._id}`);
 });
+order.get("/addWishList/:id", async (req, res) => {
+  // console.log(req.params.id);
+  let d = await data.collection("products").findOne({ _id: new ObjectId(req.params.id) });
+  let wish = {
+    name: d.name,
+    price: d.price,
+    productimage: d.productimage,
+    user: req.session.email,
+  };
+  let existUser = await data.collection("wishlist").findOne({$and:[{ user: wish.user },{name:wish.name}]});
+//   console.log(existUser)
+ 
+
+  if (existUser) {
+    console.log("already add in wishlist");
+  } else await data.collection("wishlist").insertOne(wish);
+
+  res.redirect("/users/productdetails/" + d._id);
+});
+
+order.get("/wishlist/remove/:id",async(req,res)=>{
+    // console.log(req.params.id);
+    await data.collection("wishlist").findOneAndDelete({_id:new ObjectId(req.params.id)});
+    res.redirect('/users/orders/wishlist');
+})
+// -----
+
+
+//  * MY ORDERS PRODUCTS ----
 order.get("/myorders/:productname", async (req, res) => {
   const productname = req.params.productname;
   // console.log(productname);
@@ -49,11 +79,8 @@ order.get("/myorders/:productname", async (req, res) => {
     // console.log(item)
   res.redirect(`/users/productdetails/${item[0].info[0]._id}`);
 });
-order.get("/wishlist/remove/:id",async(req,res)=>{
-    // console.log(req.params.id);
-    await data.collection("wishlist").findOneAndDelete({_id:new ObjectId(req.params.id)});
-    res.redirect('/users/orders/wishlist');
-})
+
+// * CART PRODUCTS ----
 
 order.get("/cart/:productname",async(req,res)=>{
     const productname=req.params.productname;
@@ -83,13 +110,48 @@ order.get("/cart",async(req,res)=>{
 
     let cartProducts=await data.collection("cart").find({user:req.session.email}).toArray();
     // console.log(cartProducts);
-    res.render("./user/cart",{products:cartProducts,wishNumber,ordersNumber});
-})
-order.get("/removecart/:id",async(req,res)=>{
+    // calculate total price of cart items
+    let totalPrice=0;
+    for (let i = 0; i < cartProducts.length; i++) {
+      totalPrice+=parseInt(cartProducts[i].price);
+    }
+    // console.log(totalPrice);
+    res.render("./user/cart",{products:cartProducts,wishNumber,ordersNumber,totalPrice});
+  })
+  order.get("/removecart/:id",async(req,res)=>{
     await data.collection("cart").findOneAndDelete({_id:new ObjectId(req.params.id)});
     res.redirect("/users/orders/cart");
+  })
+
+// ----
+
+//  * ORDER ALL CART PRODUCTS ----
+  order.get("/orderAll",async(req,res)=>{
+    let cartNumber=await data.collection("cart").find({user:req.session.email}).count();
+    let wishNumber=await data.collection("wishlist").find({user:req.session.email}).count();
+    let ordersNumber=await data.collection("orders").find({primaryEmail:req.session.email}).count();
+
+  let cartProducts=await data.collection("cart").find({user:req.session.email}).toArray();
+  console.log(cartProducts);
+  let totalAmount = 0;
+    cartProducts.forEach((product) => {
+      totalAmount += parseInt(product.price);
+    });
+  console.log(totalAmount);
+  let productImages = cartProducts.map(product => product.productimage);
+  let productNamess = cartProducts.map(product => product.name);
+
+  let details={
+    name:productNamess,
+    price:totalAmount,
+    productimage:productImages,
+    user:req.session.email
+  }
+  console.log(details);
+  res.render("./user/orderdetails", { product: details,cartNumber,wishNumber,ordersNumber });
 })
 
+// * ORDER SINGLE PRODUCT ------
 order.get("/ordercart/:name",async(req,res)=>{
     let item=await data.collection("cart").aggregate([
         {$match: { name : req.params.name}},
@@ -124,6 +186,8 @@ order.get("/:id", async (req, res) => {
   let product = await data.collection("products").findOne({ _id: new ObjectId(req.params.id) });
   res.render("./user/orderDetails", { product: product,cartNumber,wishNumber,ordersNumber });
 });
+
+// * ORDER USER CREDENTIALS ----
 order.post("/:id", async (req, res) => {
     const orderData =await data.collection("products").findOne({_id:new ObjectId(req.params.id)});
     console.log(orderData);
@@ -156,25 +220,7 @@ order.post("/:id", async (req, res) => {
   res.render('./user/confirmOrder');
 });
 
-order.get("/addWishList/:id", async (req, res) => {
-  // console.log(req.params.id);
-  let d = await data.collection("products").findOne({ _id: new ObjectId(req.params.id) });
-  let wish = {
-    name: d.name,
-    price: d.price,
-    productimage: d.productimage,
-    user: req.session.email,
-  };
-  let existUser = await data.collection("wishlist").findOne({$and:[{ user: wish.user },{name:wish.name}]});
-//   console.log(existUser)
- 
 
-  if (existUser) {
-    console.log("already add in wishlist");
-  } else await data.collection("wishlist").insertOne(wish);
-
-  res.redirect("/users/productdetails/" + d._id);
-});
 
 
 
