cd54f1f27179d718fcb53bd6652f9d4fe017ecf2
sujayghoshal <92462200+sujoyghoshal@users.noreply.github.com>
2023-12-04 17:02:15 +0530

    Add Ui and Brands filter

--- mongodb/connection.js
+++ mongodb/connection.js
@@ -1,13 +1,11 @@
 // mongodb connection
 const client = require("mongodb").MongoClient;
-
 const URL = "mongodb://127.0.0.1:27017";
 function connect(callback) {
   client
     .connect(URL)
     .then((database) => {
       let data = database.db("e-comm");
-      console.log("Connect database");
       callback(data);
     })
     .catch((err) => {
@@ -18,25 +16,3 @@ function connect(callback) {
 module.exports = function getData(callback){
   connect(callback);
 };
-
-// mongodb connection
-// const client = require("mongodb").MongoClient;
-// let data;
-// const URL = "mongodb://127.0.0.1:27017";
-// function connect(callback) {
-//   client
-//     .connect(URL)
-//     .then((database) => {
-//        data = database.db("e-comm");
-//       console.log("Connect database");
-//       callback(data);
-//     })
-//     .catch((err) => {
-//       console.log(err);
-//       callback(null);
-//     });
-// }
-// function getData(){
-//   return data;
-// }
-// module.exports = {connect,getData};
